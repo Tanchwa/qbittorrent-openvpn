601bd4c69b2a3bf6cb29171af94cf138602ee423
Tanchwa <89235018+Tanchwa@users.noreply.github.com>
2023-11-24 12:05:55 -0500

    Update start.sh
    didn't finish commenting out all lines needed

--- openvpn/start.sh
+++ openvpn/start.sh
@@ -81,8 +81,8 @@ if [[ ${VPN_ENABLED} == "yes" ]]; then
   		# auth_cred_exist=$(cat ${VPN_CONFIG} | grep -m 1 'auth-user-pass')
 		# if [[ -n ${auth_cred_exist} ]]; then
 		#	# get line number of auth-user-pass
-			LINE_NUM=$(grep -Fn -m 1 'auth-user-pass' ${VPN_CONFIG} | cut -d: -f 1)
-			sed -i'' "${LINE_NUM}s/.*/auth-user-pass credentials.conf\n/" ${VPN_CONFIG}
+		#	LINE_NUM=$(grep -Fn -m 1 'auth-user-pass' ${VPN_CONFIG} | cut -d: -f 1)
+		#	sed -i'' "${LINE_NUM}s/.*/auth-user-pass credentials.conf\n/" ${VPN_CONFIG}
 		# else
 		#	sed -i'' "1s/.*/auth-user-pass credentials.conf\n/" ${VPN_CONFIG}
 		# fi
